6bccec2c817d131d88f0789838d3ac5a64abd462
Batuhan Ham <batu@Batuhan-MacBook-Air.local>
2024-01-18 21:58:23 +0300

    main değiştirildi

--- src/SpaceInvaders.java
+++ src/SpaceInvaders.java
@@ -1,5 +1,7 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("askflhajhsfjasfjasfj asjfojasfjasof");
+        System.out.println("dsdngdsjkngsd");
+        //kfjaskfjlafjlaskjfa
     }
 }
